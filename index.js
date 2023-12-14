90ee6278fdab2ecd4143542fb4c132cf12d52c2b
DANIEL_GRADY <anakria977@gmail.com>
2023-12-14 10:44:11 -0500

    Finished part 1.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  // YOUR CODE HERE
+	// YOUR CODE HERE
+	// Create references to lights and buttons.
+	const stopLight = document.querySelector('#stopLight');
+	const slowLight = document.querySelector('#slowLight');
+	const goLight = document.querySelector('#goLight');
+	const stopButton = document.querySelector('#stopButton');
+	const slowButton = document.querySelector('#slowButton');
+	const goButton = document.querySelector('#goButton');
+
+	// Function that adds event listeners to toggle css class on click.
+	const addToggleLights = () => {
+		stopButton.addEventListener('click', () => {
+			stopLight.classList.toggle('stop');
+		});
+		slowButton.addEventListener('click', () => {
+			slowLight.classList.toggle('slow');
+		});
+		goButton.addEventListener('click', () => {
+			goLight.classList.toggle('go');
+		});
+	}
+	addToggleLights(); // Invoke function.
 })();
